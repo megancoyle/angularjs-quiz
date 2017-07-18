c88e5c004e59b30e8b507a10bf664ae5112bd47e
Megan Coyle <meg@mcoyle.com>
2017-07-17 21:29:18 -0400

    Complete quiz

--- js/quiz.js
+++ js/quiz.js
@@ -13,6 +13,46 @@
       $scope.myQuestions = quizData.data;
       $scope.totalQuestions = $scope.myQuestions.length;
     });
+
+    $scope.selectAnswer = function(qIndex, aIndex) {
+      var questionState = $scope.myQuestions[qIndex].questionState;
+
+      if(questionState != 'answered') {
+        $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+        var correctAnswer = $scope.myQuestions[qIndex].correct;
+        $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+
+        if(aIndex === correctAnswer) {
+          $scope.myQuestions[qIndex].correctness = 'correct';
+          $scope.score += 1;
+        } else {
+          $scope.myQuestions[qIndex].correctness = 'incorrect';
+        }
+        $scope.myQuestions[qIndex].questionState = 'answered';
+      }
+      $scope.percentage = ($scope.score / $scope.totalQuestions) * 100;
+    }
+
+    $scope.isSelected = function(qIndex, aIndex) {
+      return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+    }
+
+    $scope.isCorrect = function(qIndex, aIndex) {
+      return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+    }
+
+    $scope.selectContinue = function() {
+      return $scope.activeQuestion += 1;
+    }
+
+    $scope.createShareLinks = function(percentage) {
+      var url = 'http://example.com';
+      var emailLink = '<a class="btn email" href="mailto:?subject=Try to beat my quiz score!&amp;body=I scored a ' + percentage + '% on this quiz about Saturn. Try to beat my score at '+ url +'">Email a friend</a>';
+      var twitterLink = '<a class="btn twitter" href="http://twitter.com/share?text=I scored a ' + percentage + '% on this quiz about Saturn. Try to beat my score at&amp;hashtags=SaturnQuiz&amp;url=' + url + '" target="_blank">Tweet your score</a>';
+      var newMarkup = emailLink + twitterLink;
+
+      return $sce.trustAsHtml(newMarkup);
+    }
   }]);
 
 })();
